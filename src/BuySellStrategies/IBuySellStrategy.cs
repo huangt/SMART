fcceb44b57b90751ce2ad9d6c66ab345fd29483e
PJensen <jensen.petej@gmail.com>
2013-04-05 20:43:16 -0400

    Added buy signal delegate

--- src/BuySellStrategies/IBuySellStrategy.cs
+++ src/BuySellStrategies/IBuySellStrategy.cs
@@ -2,13 +2,14 @@ using System;
 
 namespace SMART.BuySellStrategies
 {
-	public delegate bool BuySignal();
+	public delegate bool BuySignal(long tick, MarketData data);
 
 	/// <summary>
 	/// I buy sell strategy.
 	/// </summary>
 	public interface IBuySellStrategy
 	{
+		bool BuyTrigger(BuySignal d);
 
 	}
 }
